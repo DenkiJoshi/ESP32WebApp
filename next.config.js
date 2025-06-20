93ffe4802ce58a93cf0b448a837435378692d81a
DenkiJoshi <faa@paralyze.main.jp>
2025-06-20 19:30:27 +0900

    Remove basePath for Vercel

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  distDir: 'docs', // ← GitHub Pages で /docs を使うための出力先指定
+  output: 'standalone',
   trailingSlash: true,
   images: {
     unoptimized: true
-  },
-  basePath: '/ESP32WebApp' // ← GitHub リポジトリ名に合わせて
+  }
 };
 
 module.exports = nextConfig;
